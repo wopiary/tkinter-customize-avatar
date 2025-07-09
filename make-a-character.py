f4cdffe1949d50b0456c6318dc11deae742dd2e5
Jonathan C. Lindo, Jr. <jonathanlindo.jr2008@gmail.com>
2025-07-09 20:26:15 +0800

    update-2-make-a-character.py
    put the options into frames and started to add more and more features

--- make-a-character.py
+++ make-a-character.py
@@ -11,44 +11,73 @@ s = ttk.Style()
 s.configure('glassesstyle.TCheckbutton', font=('Helvetica', 14, 'bold'), foreground="#2C2C2C")
 s.configure('hatstyle.TCheckbutton', font=('Helvetica', 14, 'bold'), foreground="#2C2C2C")
 s.configure('beardstyle.TCheckbutton', font=('Helvetica', 14, 'bold'), foreground="#2C2C2C")
-
+s.configure('male.TRadiobutton', font=('Helvetica', 14, 'bold'), foreground="#2C2C2C")
+s.configure('female.TRadiobutton', font=('Helvetica', 14, 'bold'), foreground="#2C2C2C")
+s.configure('colorthemelabel.TLabel', font=('Helvetica', 14, 'bold'), foreground="#2C2C2C")
 #LABEL
 label = tk.Label(window, text='Tkinter Avatar Customizer', font=('Inter', 20, 'bold'), fg="#4A4A4A")
-label.pack()
+label.pack(pady=10)
 
 
+#FRAMES
+top_frame = ttk.Frame(window)
+top_frame.pack(pady=20)
+
+accessory_frame = ttk.Frame(top_frame)
+accessory_frame.pack(side=tk.LEFT, padx=30)
+
+canvas_frame=ttk.Frame(top_frame)
+canvas_frame.pack(side=tk.LEFT, padx=30)
+
+genderandcolor_frame=ttk.Frame(top_frame)
+genderandcolor_frame.pack(side=tk.LEFT, padx=30)
+
+colortheme_frame = ttk.Frame(top_frame)
+colortheme_frame.pack(side=tk.LEFT, padx=2)
+
+agenameandsaveoptions_frame=  ttk.Frame(top_frame)
+agenameandsaveoptions_frame.pack(side=tk.LEFT, padx=30)
+
+#CANVAS
+canvas = tk.Canvas(canvas_frame, height=300, width=300, bg='white')
+canvas.pack()
 
 
 #ACCESSORIES
-
 glasses_var = tk.BooleanVar()
-glasses_button = ttk.Checkbutton(window, text='Glasses', variable=glasses_var, style='glassesstyle.TCheckbutton')
-glasses_button.pack(side=tk.TOP, anchor=tk.NW, pady=5, padx=80)
+glasses_button = ttk.Checkbutton(accessory_frame, text='Glasses', variable=glasses_var, style='glassesstyle.TCheckbutton')
+glasses_button.pack(anchor=tk.W, pady=5)
 
 hat_var = tk.BooleanVar()
-hat_button = ttk.Checkbutton(window, text='Hat', variable=hat_var, style='hatstyle.TCheckbutton')
-hat_button.pack(side=tk.TOP, anchor=tk.NW, pady=5, padx=80)
+hat_button = ttk.Checkbutton(accessory_frame, text='Hat', variable=hat_var, style='hatstyle.TCheckbutton')
+hat_button.pack(anchor=tk.W, pady=5)
 
 
 beard_var = tk.BooleanVar()
-beard_button = ttk.Checkbutton(window, text='Beard', variable=beard_var, style='beardstyle.TCheckbutton')
-beard_button.pack(side=tk.TOP, anchor=tk.NW, pady=5, padx=80)
-
-
+beard_button = ttk.Checkbutton(accessory_frame, text='Beard', variable=beard_var, style='beardstyle.TCheckbutton')
+beard_button.pack(anchor=tk.W, pady=5)
 
 
 #GENDER
+gender_var = tk.StringVar()
+male_button = ttk.Radiobutton(genderandcolor_frame, value='male', variable=gender_var, text='Male', style='male.TRadiobutton')
+male_button.pack(anchor=tk.E, pady=0, padx=22)
 
-male_button = ttk.Radiobutton(window, value='male', text='Male')
-male_button.pack(side=tk.TOP, anchor=tk.NE, pady=10, padx=82)
-
-female_button = ttk.Radiobutton(window, value='female', text='Female')
-female_button.pack(side=tk.TOP, anchor=tk.NE, pady=10, padx=70)
-
-
+female_button = ttk.Radiobutton(genderandcolor_frame, value='female', variable=gender_var,text='Female', style='female.TRadiobutton')
+female_button.pack(anchor=tk.E, pady=15)
 
 
+#COLOR THEMES
+colortheme_label = ttk.Label(genderandcolor_frame, text='Color theme:', style='colorthemelabel.TLabel')
+colortheme_label.pack(anchor=tk.E)
+colortheme_items = ('BLUE', 'RED', 'GREEN', 'YELLOW', 'WHITE', 'BLACK')
+colortheme = ttk.Spinbox(genderandcolor_frame, values=colortheme_items)
+colortheme.pack(anchor=tk.E)
 
+
+#AGE
+age_spinbox_items = 1 +=  
+age_spinbox = ttk.Spinbox(agenameandsaveoptions_frame, )
 
 
 
@@ -79,4 +108,6 @@ female_button.pack(side=tk.TOP, anchor=tk.NE, pady=10, padx=70)
 
 
 window.mainloop()
+
+
 
