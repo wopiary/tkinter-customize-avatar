f7facdce8de0f5af55b13f68b046ece8f88904d0
Jonathan C. Lindo, Jr. <jonathanlindo.jr2008@gmail.com>
2025-07-11 20:43:58 +0800

    update-4-make-a-character.py
    organized the code a lil more. added the functions for the male button. clicking on it creates a rectangle as of now, will make it better if i got time

--- make-a-character.py
+++ make-a-character.py
@@ -6,7 +6,7 @@ window = tk.Tk()
 window.geometry('800x800')
 window.title('Make a Character')
 
-    
+#STYLES   
 s = ttk.Style()
 s.configure('glassesstyle.TCheckbutton', font=('Helvetica', 14, 'bold'), foreground="#2C2C2C")
 s.configure('hatstyle.TCheckbutton', font=('Helvetica', 14, 'bold'), foreground="#2C2C2C")
@@ -16,12 +16,21 @@ s.configure('female.TRadiobutton', font=('Helvetica', 14, 'bold'), foreground="#
 s.configure('colorthemelabel.TLabel', font=('Helvetica', 12, 'bold'), foreground="#2C2C2C")
 s.configure('agelabel.TLabel', font=('Helvetica', 12, 'bold'), foreground="#2C2C2C")
 s.configure('namelabel.TLabel', font=('Helvetica', 12, 'bold'), foreground="#2C2C2C")
+
+
+
+
+
 #LABEL
 label = tk.Label(window, text='Tkinter Avatar Customizer', font=('Inter', 20, 'bold'), fg="#4A4A4A")
 label.pack(pady=10)
 
 
+
+
+
 #FRAMES
+#TOP FRAME
 top_frame = ttk.Frame(window)
 top_frame.pack(pady=20)
 
@@ -35,20 +44,29 @@ genderandcolor_frame=ttk.Frame(top_frame)
 genderandcolor_frame.pack(side=tk.LEFT, padx=30)
 
 
+
+
+
+#BOTTOM FRAME
 bottom_frame =ttk.Frame(window)
 bottom_frame.pack(pady=20, padx=10)
 
 ageanamelabelandsavebutton_frame = ttk.Frame(bottom_frame)
 ageanamelabelandsavebutton_frame.pack(side=tk.LEFT, padx=20)
 
-agespinboxandnameentry_frame =ttk.Frame(bottom_frame)
-agespinboxandnameentry_frame.pack(side=tk.LEFT, padx=20)
+agespinboxnameentryandresetbutton_frame =ttk.Frame(bottom_frame)
+agespinboxnameentryandresetbutton_frame.pack(side=tk.LEFT, padx=20)
+
+
 
 
 
 #CANVAS
 canvas = tk.Canvas(canvas_frame, height=300, width=300, bg='white')
-canvas.pack()
+canvas.pack()   
+
+
+
 
 
 #ACCESSORIES
@@ -66,13 +84,20 @@ beard_button = ttk.Checkbutton(accessory_frame, text='Beard', variable=beard_var
 beard_button.pack(anchor=tk.W, pady=5)
 
 
+
+
+
 #GENDER
 gender_var = tk.StringVar()
 male_button = ttk.Radiobutton(genderandcolor_frame, value='male', variable=gender_var, text='Male', style='male.TRadiobutton')
 male_button.pack(anchor=tk.E, pady=0, padx=22)
+male_button.bind('<Button-1>', lambda event: canvas.create_rectangle(170,250, 190,200))
 
 female_button = ttk.Radiobutton(genderandcolor_frame, value='female', variable=gender_var,text='Female', style='female.TRadiobutton')
 female_button.pack(anchor=tk.E, pady=15)
+
+
+
 
 
 #COLOR THEMES
@@ -83,26 +108,39 @@ colortheme = ttk.Spinbox(genderandcolor_frame, values=colortheme_items)
 colortheme.pack(anchor=tk.E)
 
 
-#AGE
 
+
+
+#AGE
 age_label = ttk.Label(ageanamelabelandsavebutton_frame, text='Age:', style='agelabel.TLabel')
 age_label.pack(anchor=tk.W)
-age_spinbox = ttk.Spinbox(agespinboxandnameentry_frame, from_=1, to=120)
+age_spinbox = ttk.Spinbox(agespinboxnameentryandresetbutton_frame, from_=1, to=120)
 age_spinbox.pack(anchor=tk.W)
+
+
+
 
 
 #NAME
 name_label = ttk.Label(ageanamelabelandsavebutton_frame, text='Name:', style='namelabel.TLabel')
 name_label.pack(anchor=tk.W)
-name_entry = ttk.Entry(agespinboxandnameentry_frame)
-name_entry.pack(anchor=tk.W)
+name_entry = ttk.Entry(agespinboxnameentryandresetbutton_frame)
+name_entry.pack(anchor=tk.W,pady=2)
+
+
+
+
 
 #SAVE 
 save_button = ttk.Button(ageanamelabelandsavebutton_frame, text='Save')
 save_button.pack(anchor=tk.W, pady=30)
 
+
+
+
+
 #RESET
-reset_button = ttk.Button(agespinboxandnameentry_frame, text='Reset')
+reset_button = ttk.Button(agespinboxnameentryandresetbutton_frame, text='Reset')
 reset_button.pack(anchor=tk.W, pady=30)
 
 
